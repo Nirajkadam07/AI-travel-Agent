f01d433e6af247087fe59954add5a78e30c7295d
Nirajkadam07 <nirajkadam2312@gmail.com>
2026-05-28 21:47:17 +0530

    updated UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,14 @@
 export default function Form() {
   return (
     <section className="form-section">
-      <p>Number of Travellers</p>
+      <div>
+        <p>Number of Travellers</p>
+        <div className="input-sec-style">
+          <button className="math-btn">-</button>
+          <span>1</span>
+          <button className="math-btn">+</button>
+        </div>
+      </div>
       <form>
         <label htmlFor="city-from-name">
           Flying from
@@ -11,11 +18,19 @@ export default function Form() {
           Flying to
           <input type="text" name="city-to" />
         </label>
+        <label htmlFor="from-date">
+          From Date
+          <input type="date" name="from-date" />
+        </label>
+        <label htmlFor="to-date">
+          To Date
+          <input type="date" name="to-date" />
+        </label>
         <label htmlFor="budget">
           Budget
           <input type="number" name="budget" />
         </label>
-        <button>Plan my trip</button>
+        <button className="submit-btn">Plan my trip!</button>
       </form>
     </section>
   );
